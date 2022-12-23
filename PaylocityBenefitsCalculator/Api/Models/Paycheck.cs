cba3055fe1444eca40a9d36e1774852343abc1f2
jvl9 <64329043+jlocicero83@users.noreply.github.com>
2022-12-23 14:14:23 -0500

    setup xUnit and begin writing tests/func

--- PaylocityBenefitsCalculator/Api/Models/Paycheck.cs
+++ PaylocityBenefitsCalculator/Api/Models/Paycheck.cs
@@ -2,42 +2,16 @@
 {
     public class Paycheck
     {
-        public Employee Employee { get; set; }
-        private int employeeAge
-        {
-            get
-            {
-                var todaysDate = DateTime.Today;
-                return todaysDate.Year - Employee.DateOfBirth.Year;
-            }
-        }
-
-        //Every employee incurs a 1K per month base cost
-        public readonly decimal MonthlyBaseCost = 1000M;
-
-        //Each dependent represents $600 per month for benefits
-        public decimal DependentsCost { get { return (decimal)600 * Employee.Dependents.Count(); } }
-
-        //if salary > 80K, incur an additional 2% of salary in costs
-        public decimal SalaryOver80kCost { get { return Employee.Salary > 80000.00M ? (Employee.Salary * 0.02M) : 0; } }
-
-        public decimal AgeOverFiftyCost { get { return employeeAge > 50 ? 200.00M : 0; } }
-
-        public decimal TotalDeductions { get { return MonthlyBaseCost + DependentsCost + SalaryOver80kCost + AgeOverFiftyCost; } }
-
-        public decimal MonthlyGross { get { return (Employee.Salary / 26); } }
-
-        public decimal MonthlyNet { get { return MonthlyGross - TotalDeductions; } }
-
+        private Employee _employee;
         public Paycheck(Employee employee)
         {
-            Employee = employee;
+            _employee = employee;
         }
+        public decimal MonthlyGross { get { return CalculateMonthlyGross(_employee.Salary); } }
 
-
-
-
-
-
+        public decimal CalculateMonthlyGross(decimal salary)
+        {
+            return Math.Round((salary / 26), 2);
+        }
     }
 }
---
+++ PaylocityBenefitsCalculator/ApiTests/BenefitsCalcTests.cs
@@ -0,0 +1,30 @@
+﻿using Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ApiTests
+{
+    public class BenefitsCalcTests
+    {
+        [Theory]
+        [InlineData(80000.00, 3076.92)]
+        [InlineData(75420.99, 2900.81)]
+        [InlineData(92365.22, 3552.51)]
+        [InlineData(143211.12, 5508.12)]
+        public void Monthly_Gross_Should_Be_Salary_DivBy_26(decimal salary, decimal expected)
+        {
+            //Arrange
+            Paycheck paycheck = new Paycheck();
+
+            //Act
+            decimal actual = paycheck.CalculateMonthlyGross(salary);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
